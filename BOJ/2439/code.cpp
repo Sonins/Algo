d303949bbd53ed43e0a5b49f4f2020d9347c15c9
Heegwan Son <gmlrhks95@gmail.com>
2020-01-22 20:57:57 +0900

    BOJ 2439 solved

---
+++ BOJ/2439/code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    for (size_t i = 1; i <= N; i++) {
+        for (size_t j = 0; j < N - i; j++)
+            cout << ' ';
+        for (size_t j = 0; j < i; j++)
+            cout << '*';
+        cout << '\n';
+    }
+    return 0;
+}
