59c0df9712d0161082995374fba148e48dd33f69
Heegwan Son <gmlrhks95@gmail.com>
2020-01-25 13:10:33 +0900

    BOJ 2445 solved

---
+++ BOJ/2445/code.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cin >> N;
+    for (size_t i = 1; i <= 2 * N - 1; i++)
+    {
+        if (i <= N) {
+            for (size_t j = 0; j < i; j++)
+                cout << '*';
+            for (size_t j = 0; j < 2 * N - 2 * i; j++)
+                cout << ' ';
+            for (size_t j = 0; j < i; j++)
+                cout << '*';
+            cout << endl;
+        }
+        else {
+            for (size_t j = 0; j < 2 * N - i; j++)
+                cout << '*';
+            for (size_t j = 0; j < 2 * i - 2 * N; j++)
+                cout << ' ';
+            for (size_t j = 0; j < 2 * N - i; j++)
+                cout << '*';
+            cout << endl;
+        }
+    }
+    return 0;
+}
