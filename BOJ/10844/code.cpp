ed7f8b04ec9275de1dd192e9e947301c60b0df16
Heegwan Son <gmlrhks95@gmail.com>
2020-01-27 14:00:27 +0900

    BOJ 10844 modified (opt)

--- BOJ/10844/code.cpp
+++ BOJ/10844/code.cpp
@@ -2,34 +2,27 @@
 #define DIV 1000000000
 using namespace std;
 int count_num[10][101];
-int memo[101];
 int main()
 {
     int N;
     cin >> N;
-    memo[1] = 9;
+    int sum = 0;
 
     for (size_t i = 1; i < 10; i++)
     {
         count_num[i][1] = 1;
     }
-    int count_0or9;
     for (size_t i = 2; i <= N; i++)
     {
-        count_num[1][i] += count_num[0][i - 1] % DIV;
-        count_num[8][i] += count_num[9][i - 1] % DIV;
-        for (size_t j = 1; j < 9; j++) {
-            count_num[j + 1][i] += count_num[j][i - 1] % DIV;
-            count_num[j - 1][i] += count_num[j][i - 1] % DIV;
-            count_num[j + 1][i] %= DIV;
-            count_num[j - 1][i] %= DIV;
-        }
-        count_0or9 = (count_num[0][i - 1] + count_num[9][i - 1]) % DIV;
-        if (memo[i - 1] * 2 > count_0or9)
-            memo[i] = (memo[i - 1] * 2 - count_0or9) % DIV;
-        else
-            memo[i] = (DIV - count_0or9 + memo[i - 1] * 2) % DIV;
+        count_num[0][i] = count_num[1][i - 1] % DIV;
+        count_num[9][i] = count_num[8][i - 1] % DIV;
+        for (size_t j = 1; j <= 8; j++)
+            count_num[j][i] = (count_num[j - 1][i - 1] + count_num[j + 1][i - 1]) % DIV;
     }
-    cout << memo[N] << endl;
+    for (size_t i = 0; i < 10; i++) {
+        sum += count_num[i][N] % DIV;
+        sum %= DIV;
+    }
+    cout << sum << endl;
     return 0;
 }
