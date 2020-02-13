15ce55697e346ef8d9bc517b0988004d1d05e57f
Heegwan Son <gmlrhks95@gmail.com>
2020-02-13 19:59:17 +0900

    BOJ 10809 minor fix

--- BOJ/10809/code.cpp
+++ BOJ/10809/code.cpp
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 int main() {
     char str[110];
-    int arr[26];
-    memset(arr, -1, sizeof(arr));
+    int arr[26] = {0};
     scanf("%s", str);
-    size_t idx = -1;
-    while(str[++idx])
-        arr[str[idx] - 'a'] = (arr[str[idx] - 'a'] == -1 ? idx : arr[str[idx] - 'a']);
+    int idx = -1;
+    while(str[++idx]) { if(!arr[str[idx] - 'a']) arr[str[idx] - 'a'] = idx + 1; }
     for (size_t i = 0; i < 26; i++)
-        printf("%d ", arr[i]);
+        printf("%d ", --arr[i]);
     printf("\n");
     return 0;
 }
