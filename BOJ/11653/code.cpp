27b432a0c71e2dc7328e7952bdd66b487274713a
Heegwan Son <gmlrhks95@gmail.com>
2020-02-26 19:57:19 +0900

    BOJ 11653 solved

--- BOJ/11653/code.cpp
+++ BOJ/11653/code.cpp
@@ -1,43 +1,21 @@
 #include <iostream>
-#include <cstring>
-#define MAX 3500
 using namespace std;
 int main() {
     ios::sync_with_stdio(false);
     cout.tie(NULL);
     int N;
     cin >> N;
-    bool is_prime[MAX];
-    memset(is_prime, true, sizeof(is_prime));
-
-    is_prime[1] = false;
-    for (size_t i = 2; i * i <= MAX; i++) {
-        if (is_prime[i]) {
-            for (size_t j = i * i; j < MAX; j += i)
-                is_prime[j] = false;
+    
+    for (size_t i = 2; i * i < 10000000; i++)
+    {
+        while (N % i == 0) {
+            cout << i << '\n';
+            N /= i;
         }
     }
+    
+    if (N > 1)
+        cout << N << '\n';
 
-    bool changed = 0;
-    while (true) {
-        for (size_t i = 2; i < MAX; i++) {
-            if (is_prime[i] && !(N % i)) {
-                cout << i << '\n';
-                N = N / i;
-                changed = true;
-                break;
-            }
-        }
-
-        if (N == 1)
-            break;
-
-        if (N > MAX && !changed) {
-            cout << N << '\n';
-            break;
-        }
-
-        changed = false;
-    }
     return 0;
 }
