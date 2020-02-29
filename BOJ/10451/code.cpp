b443762b7b67fc612aa811ebf4fc90353ffe2dca
Heegwan Son <gmlrhks95@gmail.com>
2020-02-29 14:10:28 +0900

    BOJ 10451 solved

---
+++ BOJ/10451/code.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int g[1010];
+bool visited[1010];
+
+void DFS(int n) {
+    visited[n] = true;
+    if (!visited[g[n]]) {
+        DFS(g[n]);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int T;
+    int N;
+    int x;
+    int cnt = 0;
+    cin >> T;
+    for (size_t t = 0; t < T; t++) {
+        cin >> N;
+        cnt = 0;
+        for (size_t i = 1; i <= N; i++) {
+            g[i] = 0;
+            visited[i] = false;
+        }
+
+        for (size_t i = 1; i <= N; i++) {
+            cin >> x;
+            g[i] = x;
+        }
+
+        for (size_t i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                DFS(i);
+                cnt++;
+            }
+        }
+        cout << cnt << '\n';
+    }
+}
