11c3b3053d97856d32eabbc2991522a3fece1706
Heegwan Son <gmlrhks95@gmail.com>
2020-03-28 12:45:31 +0900

    BOJ 2875 temp save

---
+++ BOJ/2875/code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int N, M, K;
+    cin >> N >> M >> K;
+    int group = 0;
+
+    if (N >= 2 * M) {
+        N -= 2 * M;
+        group += M;
+        K -= N;
+    } else {
+        M -= N / 2;
+        group += N / 2;
+        N -= N / 2;
+        K -= N + M;
+    }
+    N = 0;
+    M = 0;
+
+    while (K > 0) {
+        group--;
+        K -= 3;
+    }
+
+    cout << group << '\n';
+    return 0;
+}
