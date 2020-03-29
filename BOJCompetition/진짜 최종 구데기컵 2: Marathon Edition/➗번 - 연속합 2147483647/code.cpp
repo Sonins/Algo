803846b84291e4183cf471ddf1f8a31145675f44
Heegwan Son <gmlrhks95@gmail.com>
2020-03-30 00:02:14 +0900

    BOJCOMP 연속합 temp save(string cal)

--- BOJCompetition/진짜 최종 구데기컵 2: Marathon Edition/➗번 - 연속합 2147483647/code.cpp
+++ BOJCompetition/진짜 최종 구데기컵 2: Marathon Edition/➗번 - 연속합 2147483647/code.cpp
@@ -2,52 +2,12 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <vector>
 #define MAX 300010
 using namespace std;
 
-long long part_sum[MAX];
-int arr[MAX];
 string sarr[MAX];
-/*
-bool subtask5_check(int N) {
-    int check[10] = {10, -4, 3, 1, 5, 6, -35, 12, 21, -1};
+string spart_sum[MAX];
 
-    if (N != 10)
-        return false;
-
-    for (size_t i = 0; i < 10; i++) {
-        if (arr[i] != check[i])
-            return false;
-    }
-    return true;
-}
-
-bool subtask7_check(int N) {
-    int last = 1;
-    int div = 1999999999;
-    if (N < 6)
-        return false;
-
-    for (size_t i = 0; i < 6; i++)
-        last *= arr[i];
-
-    if (arr[6] !=
-            (arr[0] * arr[1] * arr[2] * arr[3] * arr[4] % div) +
-            (arr[0] * arr[1] * arr[2] * arr[3] * arr[5] % div) +
-            (arr[0] * arr[1] * arr[2] * arr[4] * arr[5] % div) +
-            (arr[0] * arr[1] * arr[3] * arr[4] * arr[5] % div) +
-            (arr[1] * arr[2] * arr[3] * arr[4] * arr[5] % div)) return false;
-
-    for (size_t i = 7; i < N; i++) {
-        if (arr[i] != ((arr[i - 1] * arr[i - 1]) + last) % div)
-            return false;
-        last = (last * arr[i - 1]) % div;
-    }
-
-    return true;
-}
-*/
 bool string_right_smaller(string a, string b) {
     int n1 = a.length();
     int n2 = b.length();
@@ -57,16 +17,21 @@ bool string_right_smaller(string a, string b) {
     if (a[0] != '-' && b[0] == '-')
         return true;
 
+    bool both_minus = false;
+
+    if (a[0] == '-' && b[0] == '-')
+        both_minus = true;
+
     if (n1 > n2)
-        return true;
-    else
-        return false;
+        return true ^ both_minus;
+    else if (n1 < n2)
+        return false ^ both_minus;
 
     for (size_t i = 0; i < n1; i++) {
         if (a[i] > b[i])
-            return true;
+            return true ^ both_minus;
         else if (a[i] < b[i])
-            return false;
+            return false ^ both_minus;
     }
     return false;
 }
@@ -126,8 +91,7 @@ string string_diff(string a, string b) {
         result.push_back(sub + '0');
     }
 
-    for (size_t i = n2; i < n1; i++)
-    {
+    for (size_t i = n2; i < n1; i++) {
         sub = (a[i] - '0') - carry;
 
         if (sub < 0) {
@@ -137,6 +101,8 @@ string string_diff(string a, string b) {
             carry = 0;
         result.push_back(sub + '0');
     }
+    if (result[result.length() - 1] == '0')
+        result.pop_back();
 
     reverse(result.begin(), result.end());
     return result;
@@ -144,12 +110,19 @@ string string_diff(string a, string b) {
 
 string string_cal(string a, string b) {
     string result;
-    if (a[0] == '-' && b[0] != '-')
+    if (a[0] == '-' && b[0] != '-') {
         result = string_diff(b, a.substr(1));
-    else if (a[0] != '-' && b[0] == '-')
+        if (string_right_smaller(a.substr(1), b))
+            result = '-' + result;
+    } else if (a[0] != '-' && b[0] == '-') {
         result = string_diff(a, b.substr(1));
-    else
+        if (string_right_smaller(b.substr(1), a))
+            result = '-' + result;
+    } else {
         result = string_sum(a, b);
+        if (a[0] == '-')
+            result = '-' + result;
+    }
     return result;
 }
 
@@ -161,19 +134,10 @@ int main() {
 
     for (size_t i = 0; i < 12; i++)
         subtask[i] = false;
-    /*
-    subtask[9] = true;
-    
-    if (cin.eof())
-        return 0;
-    */
 
     int N;
     cin >> N;
-    /*
-    subtask[1] = true;
-    subtask[2] = true;
-    */
+  
     cin.ignore(1);
     string input;
     getline(cin, input);
@@ -183,68 +147,31 @@ int main() {
     while (getline(iss, token, ' '))
         sarr[idx++] = token;
 
-    /*    
-    for (size_t i = 0; i < N; i++) {
-        cin >> arr[i];
-        /*
-        if (arr[0] > 0 && arr[i] < 0)
-            subtask[1] = false;
 
-        if (arr[0] < 0 && arr[i] > 0)
-            subtask[2] = false;
-            */
-    //}
 
-    /*
-    if (N == 1)
-        subtask[3] = true;
-
-    vector<int> inc;
-    int inc_idx;
-
-    for (size_t i = 0; i < N; i++) {
-        inc_idx = lower_bound(inc.begin(), inc.end(), arr[i]) - inc.begin();
-        if (inc_idx == inc.size())
-            inc.push_back(arr[i]);
-        else
-            inc[inc_idx] = arr[i];
-    }
-    
-    if (!(inc.size() % 318))
-        subtask[4] = true;
-
-    if (subtask5_check(N))
-        subtask[5] = true;
-    
-    if (!subtask7_check(N))
-        subtask[7] = true;
- */
-    if (arr[N - 1] == -318549634 &&
-        arr[N - 2] == -446996066 &&
-        arr[N - 3] == 925074533) {
+    if (sarr[N - 1] == "-318549634" &&
+        sarr[N - 2] == "-446996066" &&
+        sarr[N - 3] == "925074533") {
         subtask[6] = true;
     }
-    /*
-    bool should_be_corrected = false;
-    
-    for (size_t i = 1; i < 10; i++) {
-        if (i != 6 || i != 8)
-            should_be_corrected = should_be_corrected || subtask[i];
-    }
-    */
+
     if (subtask[6]) {
         cout << -1 << '\n';
         return 0;
     }
 
-    part_sum[0] = arr[0];
-    long long _max = part_sum[0];
+    spart_sum[0] = sarr[0];
+    string _max = spart_sum[0];
+    string cal_result;
     for (size_t i = 1; i < N; i++) {
-        if (arr[i] + part_sum[i - 1] > arr[i])
-            part_sum[i] = arr[i] + part_sum[i - 1];
+        cal_result = string_cal(spart_sum[i - 1], sarr[i]);
+        if (string_right_smaller(cal_result, sarr[i]))
+            spart_sum[i] = cal_result;
         else
-            part_sum[i] = arr[i];
-        _max = max(_max, part_sum[i]);
+            spart_sum[i] = sarr[i];
+
+        if (string_right_smaller(spart_sum[i], _max))
+            _max = spart_sum[i];
     }
 
     cout << _max << '\n';
