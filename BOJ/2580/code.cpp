a387e4a1d8adce92f310ea3a08c8bcace63dc91a
Son Heegwan <gmlrhks95@gmail.com>
2020-07-09 13:48:32 +0900

    BOJ 2580 opt

--- BOJ/2580/code.cpp
+++ BOJ/2580/code.cpp
@@ -1,74 +1,76 @@
 #include <string.h>
 
-#include <iostream>
+#include <cstdio>
 #include <vector>
 
 using namespace std;
 
+bool row[9][10], col[9][10], sq[9][10];
 int sdoku[9][9];
+vector<pair<int, int> > blank;
 
-bool DFS(int x, int y) {
+int idx_to_sq(int x, int y) {
+    return x / 3 * 3 + y / 3;
+}
+
+bool DFS(int pos) {
+    if (pos == blank.size())
+        return true;
+
     bool ret = false;
-    bool cand[10];
-    memset(cand, 1, sizeof(cand));
 
-    for (int i = 0; i < 9; i++) {
-        if (sdoku[x][i])
-            cand[sdoku[x][i]] = false;
-        if (sdoku[i][y])
-            cand[sdoku[i][y]] = false;
-        if (sdoku[i / 3 + (x / 3) * 3][(i % 3) + (y / 3) * 3])
-            cand[sdoku[i / 3 + (x / 3) * 3][(i % 3) + (y / 3) * 3]] = false;
-    }
+    int x = blank[pos].first;
+    int y = blank[pos].second;
 
-    for (int i = 0; i < 9; i++) {
-        if (!cand[i + 1])
+    for (int i = 1; i <= 9; i++) {
+        if (!row[x][i] || !col[y][i] || !sq[idx_to_sq(x, y)][i])
             continue;
 
         ret = true;
-        sdoku[x][y] = i + 1;
 
-        for (int j = 0; j < 9; j++) {
-            for (int k = 0; k < 9; k++) {
-                if (!sdoku[j][k]) {
-                    if (!DFS(j, k)) {
-                        ret = false;
-                        break;
-                    }
-                }
-            }
-            if (!ret)
-                break;
-        }
+        sdoku[x][y] = i;
+        row[x][i] = false;
+        col[y][i] = false;
+        sq[idx_to_sq(x, y)][i] = false;
+
+        ret = DFS(pos + 1);
 
         if (ret)
             break;
-        else
+        else {
             sdoku[x][y] = 0;
+            row[x][i] = true;
+            col[y][i] = true;
+            sq[idx_to_sq(x, y)][i] = true;
+        }
     }
     return ret;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++)
-            cin >> sdoku[i][j];
-    }
+    memset(row, 1, sizeof(row));
+    memset(col, 1, sizeof(col));
+    memset(sq, 1, sizeof(sq));
 
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
+            scanf("%d", &sdoku[i][j]);
             if (!sdoku[i][j])
-                DFS(i, j);
+                blank.push_back(make_pair(i, j));
+            else {
+                row[i][sdoku[i][j]] = false;
+                col[j][sdoku[i][j]] = false;
+                sq[idx_to_sq(i, j)][sdoku[i][j]] = false;
+            }
         }
     }
 
+    DFS(0);
+
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++)
-            cout << sdoku[i][j] << " ";
-        cout << '\n';
+            printf("%d ", sdoku[i][j]);
+        printf("\n");
     }
 
     return 0;
