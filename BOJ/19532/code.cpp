8f0e96b5fcfcf9166a20ef684c3987a871f12afa
Son Heegwan <gmlrhks95@gmail.com>
2020-07-28 23:18:12 +0900

    BOJ 19532 solved

---
+++ BOJ/19532/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    for (int i = -999; i < 1000; i++) {
+        for (int j = -999; j < 1000; j++) {
+            if (a * i + b * j == c && d * i + e * j == f) {
+                cout << i << " " << j << '\n';
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
