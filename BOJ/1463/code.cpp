250fbaba45e083e1b5727e7618fdf51301c946e2
Son Heegwan <gmlrhks95@gmail.com>
2021-02-18 23:10:59 +0900

    BOJ 1463 resolved(data added)

--- BOJ/1463/code.cpp
+++ BOJ/1463/code.cpp
@@ -9,12 +9,12 @@ int main() {
     memo[2] = 1;
     memo[3] = 1;
     for (int i = 4; i <= N; i++) {
+        memo[i] = memo[i - 1] + 1;
         if (i % 3 == 0)
-            memo[i] = min(memo[i / 3] + 1, memo[i - 1] + 1);
-        else if (i % 2 == 0)
-            memo[i] = min(memo[i / 2] + 1, memo[i - 1] + 1);
-        else
-            memo[i] = memo[i - 1] + 1;
+            memo[i] = min(memo[i / 3] + 1, memo[i]);
+        if (i % 2 == 0)
+            memo[i] = min(memo[i / 2] + 1, memo[i]);
+        
     }
     cout << memo[N] << '\n';
     return 0;
