1843dfe8c9e8165ada70cf05b4890cd3e733a2d9
Sonins <gmlrhks95@gmail.com>
2022-01-09 12:19:30 +0900

    BOJ 1149 code rearranged.

--- BOJ/1149/code.cpp
+++ BOJ/1149/code.cpp
@@ -1,27 +1,24 @@
 #include <stdio.h>
 
-using namespace std;
-
 #define MAX 1010
-
-int color[MAX][3];
 
 int min(int a, int b) { return a < b ? a : b; }
 
 int main() {
     int N;
+    int c[MAX][3];
     scanf("%d", &N);
 
     for (int i = 0; i < N; i++) {
-        scanf("%d %d %d", &color[i][0], &color[i][1], &color[i][2]);
+        scanf("%d %d %d", &c[i][0], &c[i][1], &c[i][2]);
     }
 
     for (int i = 1; i < N; i++) {
-        color[i][0] = min(color[i - 1][1], color[i - 1][2]) + color[i][0];
-        color[i][1] = min(color[i - 1][0], color[i - 1][2]) + color[i][1];
-        color[i][2] = min(color[i - 1][0], color[i - 1][1]) + color[i][2];
+        c[i][0] = min(c[i - 1][1], c[i - 1][2]) + c[i][0];
+        c[i][1] = min(c[i - 1][0], c[i - 1][2]) + c[i][1];
+        c[i][2] = min(c[i - 1][0], c[i - 1][1]) + c[i][2];
     }
 
-    printf("%d\n", min(min(color[N - 1][0], color[N - 1][1]), color[N - 1][2]));
+    printf("%d\n", min(min(c[N - 1][0], c[N - 1][1]), c[N - 1][2]));
     return 0;
 }
