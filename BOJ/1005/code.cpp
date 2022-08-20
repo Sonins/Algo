c5d3bfd6087b70883c97631977a48ce6adc57b83
Heegwan Son <hson@bearrobotics.ai>
2022-08-20 10:21:28 -0500

    BOJ 1005 solved

---
+++ BOJ/1005/code.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <set>
+#include <string.h>
+
+#define MAX 100010
+using namespace std;
+
+int delay[MAX];
+int before[MAX];
+int dep[MAX];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+    int T, N, K, W;
+
+    cin >> T;
+    while(T--) {
+        memset(before, 0, sizeof(int) * MAX);
+        cin >> N >> K;
+        for (int i = 1; i <= N; i++) {
+            cin >> delay[i];
+        }
+
+        int parent, child;
+
+        set<int> edge[MAX];
+        for (int i = 0; i < K; i++) {
+            cin >> parent >> child;
+            edge[parent].insert(child);
+            dep[child]++;
+        }
+        cin >> W;
+
+        stack<int> st;
+        for (int i = 1; i <= N; i++) {
+            if (dep[i] == 0) st.push(i);
+        }
+
+        while (!st.empty()) {
+            int cur = st.top();
+            st.pop();
+
+            for (auto i : edge[cur]) {
+                before[i] = max(before[i], before[cur] + delay[cur]);
+                dep[i]--;
+                if (dep[i] == 0) st.push(i);
+            }
+        }
+        cout << before[W] + delay[W] << '\n';
+    }
+
+}
